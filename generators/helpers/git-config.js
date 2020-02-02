45db46d297c693a46a10b39d9d9b767a51ce054f
Mike <mikenikles@gmail.com>
2020-02-02 18:31:19 +0000

    Add a gitConfig helper.

---
+++ generators/helpers/git-config.js
@@ -0,0 +1,14 @@
+const merge = require("lodash/merge");
+const get = require("lodash/get");
+const getGitConfigPath = require("git-config-path");
+const parseGitConfig = require("parse-git-config");
+
+const globalGitConfig = parseGitConfig.sync({
+  cwd: "/",
+  path: getGitConfigPath({ type: "global" })
+});
+const localGitConfig = parseGitConfig.sync();
+const gitConfig = merge({}, globalGitConfig, localGitConfig);
+console.log(gitConfig)
+
+module.exports = configPath => get(gitConfig, configPath);
--- generators/index.js
+++ generators/index.js
@@ -1,5 +1,7 @@
+const gitConfigHelper = require("./helpers/git-config");
 const serviceGenerator = require("./service");
 
 module.exports = function(plop) {
+  plop.setHelper("gitConfig", gitConfigHelper);
   plop.setGenerator("service", serviceGenerator(plop));
 };
